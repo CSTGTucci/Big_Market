ee3cf509aa76031406927308c9fbc519e29f0086
Tucci <27198683@qq.com>
2024-09-09 16:51:23 +0800

    feat:9.09 完成分库分表，并测试

---
+++ BM-infrastructure/src/main/java/org/example/infrastructure/persistent/dao/IRaffleActivityDao.java
@@ -0,0 +1,13 @@
+package org.example.infrastructure.persistent.dao;
+
+
+import org.apache.ibatis.annotations.Mapper;
+import org.example.infrastructure.persistent.po.RaffleActivity;
+
+
+@Mapper
+public interface IRaffleActivityDao {
+
+    RaffleActivity queryRaffleActivityByActivityId(Long activityId);
+
+}
